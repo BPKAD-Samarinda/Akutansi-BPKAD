f96134af2b1d1f7ca85f3e7bf83fed812617df14
Asnan Fadjri Wahyudi <asnanfadjriw@gmail.com>
2026-03-10 21:30:42 +0800

    Style upload page and localize titles

--- frontend/src/pages/UploadHistory.tsx
+++ frontend/src/pages/UploadHistory.tsx
@@ -90,7 +90,7 @@ export default function UploadHistory() {
       <Sidebar />
 
       <div className="ml-20 lg:ml-[88px] flex-1 flex flex-col">
-        <Header title="File History" />
+        <Header title="Riwayat Unggah" />
 
         <main className="flex-1 p-1 md:p-8" data-history-animate>
           <div className="mx-auto w-full max-w-none space-y-6" data-history-animate>
